bc5571db80d989a4ffb8bf12e8a36643d50f568a
Karan Kale <karan.kale@gmail.com>
2025-04-16 14:53:38 +0530

    Fix terraform vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,4 +5,5 @@ variable "aws_region" {
 variable "eks_admin_iam_principal_arn" {
   type        = string
   description = "The IAM principal to be granted cluster admin access"
+  default     = "arn:aws:iam::079892728706:root"
 }
